b2fd06bdcf91345b01f7814a05065be43514451b
KayterCantFixBug <kienductrong@gmail.com>
2024-03-27 21:26:30 +0700

    a

---
+++ DAO_21110332_270324/src/main/java/dao/DAOFactory.java
@@ -0,0 +1,19 @@
+package dao;
+
+import dao.mysql.MysqlDataSourceDAOFactory;
+
+public abstract class DAOFactory {
+
+	public static final int MySQLDataSource = 1;
+
+	public abstract BooksDAO getBooksDAO();
+
+	public static DAOFactory getDAOFactory(int whichFactory) {
+		switch (whichFactory) {
+		case MySQLDataSource:
+			return MysqlDataSourceDAOFactory.getInstance();
+		default:
+			return null;
+		}
+	}
+}
